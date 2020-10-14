30f70617f74e9fa46de16b84ed68e3848a9979ec
Daniel Nascimento <danielhn@gmail.com>
2020-10-14 14:50:47 -0400

    Firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBFmaiGqiHXBtbEbz4ifmnYaYzl_CaFXJQ",
+  authDomain: "product-hunt-fbc21.firebaseapp.com",
+  databaseURL: "https://product-hunt-fbc21.firebaseio.com",
+  projectId: "product-hunt-fbc21",
+  storageBucket: "product-hunt-fbc21.appspot.com",
+  messagingSenderId: "657979606218",
+  appId: "1:657979606218:web:2199d7797d43ac7227a033",
+  measurementId: "G-6CV8DCF0JX",
+};
+
+export default firebaseConfig;
